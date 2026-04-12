0c3c4debe72162435b20b1486680d7c42adb50a2
hoopstreet <hoopstreet143@gmail.com>
2026-04-12 16:53:58 +0000

    fix: nuclear patch for all voice IDs to stop undefined crashes

--- src/short-creator/libraries/Kokoro.ts
+++ src/short-creator/libraries/Kokoro.ts
@@ -1,9 +1,9 @@
-import { VoiceId } from "../../types/shorts";
+import { ''} from "../../types/shorts";
 
 export const KOKORO_VOICES = [
   { id: 'fish_ate_budol', name: 'Ate (Fish: Realistic Taglish)', lang: 'fil-PH', engine: 'fish-speech' },
   { id: 'fish_kuya_tech', name: 'Kuya (Fish: Energetic Pinoy)', lang: 'fil-PH', engine: 'fish-speech' },
-  { id: VoiceId.af_heart, name: 'Heart', lang: 'en-US', engine: 'kokoro' },
-  { id: VoiceId.af_bella, name: 'Bella', lang: 'en-US', engine: 'kokoro' },
-  { id: VoiceId.am_adam, name: 'Adam', lang: 'en-US', engine: 'kokoro' }
+  { id: 'af_heart', name: 'Heart', lang: 'en-US', engine: 'kokoro' },
+  { id: 'af_bella', name: 'Bella', lang: 'en-US', engine: 'kokoro' },
+  { id: 'am_adam', name: 'Adam', lang: 'en-US', engine: 'kokoro' }
 ];
