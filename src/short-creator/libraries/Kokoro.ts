6bc99ef39ba5549ca275d569481d5600854d96ae
hoopstreet <hoopstreet143@gmail.com>
2026-04-12 16:35:29 +0000

    chore: force voiceId string and update enum

--- src/short-creator/libraries/Kokoro.ts
+++ src/short-creator/libraries/Kokoro.ts
@@ -1,7 +1,7 @@
 import { VoiceId } from "../../types/shorts";
 
 export const KOKORO_VOICES = [
-  { id: VoiceId.fish_ate_budol, name: 'Ate (Fish: Realistic Taglish)', lang: 'fil-PH', engine: 'fish-speech' },
+  { id: 'fish_ate_budol', name: 'Ate (Fish: Realistic Taglish)', lang: 'fil-PH', engine: 'fish-speech' },
   { id: VoiceId.fish_kuya_tech, name: 'Kuya (Fish: Energetic Pinoy)', lang: 'fil-PH', engine: 'fish-speech' },
   { id: VoiceId.af_heart, name: 'Heart', lang: 'en-US', engine: 'kokoro' },
   { id: VoiceId.af_bella, name: 'Bella', lang: 'en-US', engine: 'kokoro' },
