a57f0cfb956bb682de148115cbe23999426258d9
hoopstreet <hoopstreet143@gmail.com>
2026-04-12 19:37:50 +0000

    feat: add Hormozi caption style and pop animations from reference video

---
+++ src/shared/constants.ts
@@ -0,0 +1,11 @@
+
+export const CAPTION_THEMES = {
+  hormozi: {
+    font: 'Montserrat-ExtraBold',
+    uppercase: true,
+    primaryColor: '#FFFFFF',
+    highlightColor: '#FFFF00', // The yellow from your video
+    outlineColor: '#000000',
+    outlineWidth: 8
+  }
+};
